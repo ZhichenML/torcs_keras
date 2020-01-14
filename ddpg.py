23eb8de3cfb11669fab5d0cd83c19adf41500225
Zhichen Gong <zhichengong@ZhichendeMacBook-Pro.local>
2020-01-14 20:31:49 +0000

    add draw

--- ddpg.py
+++ ddpg.py
@@ -172,17 +172,17 @@ def playGame(train_indicator=1):    #1 means Train, 0 means simply Run
         save_toal_step.append(step)
 
         if np.mod(i, 100) == 0:
-            filename = "/Fig/save_total_reward.pickle"
-            dirname = os.path.dirname(filename)
-            if not os.path.exists(dirname):
-                os.makedirs(dirname)
+            filename = "save_total_reward.pickle"
+            #dirname = os.path.dirname(filename)
+            #if not os.path.exists(dirname):
+            os.mkdir(filename)
             with open(filename,'wb') as f:
                 pickle.dump(save_total_reward, f)
 
-            filename = "/Fig/save_total_step.pickle"
-            dirname = os.path.dirname(filename)
-            if not os.path.exists(dirname):
-                os.makedirs(dirname)
+            filename = "save_total_step.pickle"
+            #dirname = os.path.dirname(filename)
+            #if not os.path.exists(dirname):
+            os.mkdir(filename)
             with open(filename,"wb") as f:
                 pickle.dump(save_toal_step, f)
 
